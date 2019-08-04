ed20736bda39dc9ab11e222383b182ac1db48847
Tomohito YABU <tyabu1212@gmail.com>
2019-08-04 15:46:40 +0900

    Add pprof.

--- webapp/go/src/torb/app.go
+++ webapp/go/src/torb/app.go
@@ -9,6 +9,8 @@ import (
 	"html/template"
 	"io"
 	"log"
+	"net/http"
+	_ "net/http/pprof"
 	"os"
 	"os/exec"
 	"sort"
@@ -310,6 +312,10 @@ func (r *Renderer) Render(w io.Writer, name string, data interface{}, c echo.Con
 var db *sql.DB
 
 func main() {
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&charset=utf8mb4",
 		os.Getenv("DB_USER"), os.Getenv("DB_PASS"),
 		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"),
