96363b546ca69596f75fa3480836249d612ec594
Tomohito YABU <tyabu1212@gmail.com>
2019-08-11 13:37:16 +0900

    トランザクション貼り忘れの修正

--- webapp/go/src/torb/app.go
+++ webapp/go/src/torb/app.go
@@ -674,15 +674,17 @@ func main() {
 		var sheet Sheet
 		var reservationID int64
 		for {
-			if err := db.QueryRow("SELECT * FROM sheets WHERE id NOT IN (SELECT sheet_id FROM reservations WHERE event_id = ? AND canceled_at IS NULL FOR UPDATE) AND `rank` = ? ORDER BY RAND() LIMIT 1", event.ID, params.Rank).Scan(&sheet.ID, &sheet.Rank, &sheet.Num, &sheet.Price); err != nil {
-				if err == sql.ErrNoRows {
-					return resError(c, "sold_out", 409)
-				}
+			tx, err := db.Begin()
+			if err != nil {
 				return err
 			}
 
-			tx, err := db.Begin()
-			if err != nil {
+			if err := tx.QueryRow("SELECT * FROM sheets WHERE id NOT IN (SELECT sheet_id FROM reservations WHERE event_id = ? AND canceled_at IS NULL FOR UPDATE) AND `rank` = ? ORDER BY RAND() LIMIT 1", event.ID, params.Rank).Scan(&sheet.ID, &sheet.Rank, &sheet.Num, &sheet.Price); err != nil {
+				tx.Rollback()
+				log.Println("rollback by", err)
+				if err == sql.ErrNoRows {
+					return resError(c, "sold_out", 409)
+				}
 				return err
 			}
 
